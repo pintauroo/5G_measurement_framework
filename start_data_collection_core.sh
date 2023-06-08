5120ac9cc3ea58f5e55e834abfc213b66cf83ff6
aashdown2001 <113197079+aashdown2001@users.noreply.github.com>
2023-06-08 13:54:14 -0400

    Update start_data_collection_core.sh

--- start_data_collection_core.sh
+++ start_data_collection_core.sh
@@ -13,9 +13,15 @@ interface=demo-oai
 iperf_duration=100s
 pcap_file_name=core.pcap
 cc_file_name=core_cc
+tcpdump_tmux_name=iperf_capture
+cc_tmux_name=cc
+iperf_client_tmux_name=iperf_client
+iperf_server_tmux_name=iperf_server
+srv_num=1
+cli_num=1
 
 # Parse command line arguments
-while getopts ":p:a:f:c:d:i:t:x:y:" opt; do
+while getopts ":p:a:f:c:d:i:t:" opt; do
   case $opt in
     p) dst_port=$OPTARG ;;
     a) dst_addr=$OPTARG ;;
@@ -24,8 +30,6 @@ while getopts ":p:a:f:c:d:i:t:x:y:" opt; do
     d) DL=$OPTARG ;;
     i) interface=$OPTARG ;;
     t) iperf_duration=$OPTARG ;;
-    x) pcap_file_name=$OPTARG ;;
-    y) cc_file_name=$OPTARG ;;
     \?) echo "Invalid option -$OPTARG" >&2 ;;
   esac
 done
@@ -33,7 +37,11 @@ done
 echo "Capturing on $interface"
 echo "Dumping iperf session data in $pcap_file_name"
 
-tmux new-session -d -s "iperf_capture" 'tcpdump -i '"$interface"' -n tcp -s 88 -w '"$pcap_file_name"'; sleep 0.00001' 
+if ! (tmux has-session -t "$iperf_capture")
+  then	
+    tmux new-session -d -s "iperf_capture" 'tcpdump -i '"$interface"' -n tcp -s 88 -w '"$pcap_file_name"'; sleep 0.00001' 
+fi
+
 
 if "$DL"
   then
@@ -45,18 +53,31 @@ if "$DL"
     echo "Dumping cc data in $cc_file_name"
     echo "downlink"
 
-    tmux new-session -d -s "cc" './ss_script.sh '"$dst_addr"' '"$frequency"' > '"$cc_file_name"'; sleep 0.00001'
-    sleep 1s
-    tmux new-session -d -s "iperf_client" iperf3 -c "$dst_addr" -t "$iperf_duration" -C "$cc" -p "$dst_port"
-    date +%H%M%S.%6N
-    
-    while tmux has-session -t "iperf_client"; do
-      sleep 10s
+    while true; do
+      if ! (tmux has-session -t "${iperf_client_tmux_name}${cli_num}")
+        then
+	  tmux new-session -d -s "${cc_tmux_name}${cli_num}" './ss_script.sh '"$dst_addr"' '"$frequency"' > '"${cc_file_name}_${cc}_DL_${cli_num}"'; sleep 0.00001'	
+          sleep 1s
+	  tmux new-session -d -s "${iperf_client_tmux_name}${cli_num}" iperf3 -c "$dst_addr" -t "$iperf_duration" -C "$cc" -p "$dst_port"
+          date +%H%M%S.%6N
+          break
+      fi
+      let "cli_num=cli_num+1"
     done
- 
+
+    while tmux has-session -t "iperf_client"; do
+      sleep 5s
+    done
     tmux kill-server
 
 else
-  tmux new-session -d -s "iperf_server" iperf3 -s -p "$dst_port"
+  while true; do
+      if ! (tmux has-session -t "${iperf_server_tmux_name}${srv_num}")
+        then
+          tmux new-session -d -s "${iperf_server_tmux_name}${srv_num}" iperf3 -s -p "$dst_port"
+          break
+      fi
+      let "srv_num=srv_num+1"
+  done
 
 fi
