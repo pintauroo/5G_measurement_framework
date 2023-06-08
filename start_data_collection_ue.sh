71d7de3265a9a62c16dc79b9404bd97a85ff5af0
aashdown2001 <113197079+aashdown2001@users.noreply.github.com>
2023-06-07 22:46:16 -0400

    Create start_data_collection_ue.sh

---
+++ start_data_collection_ue.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#Add route to core
+route add default gw 12.1.1.1
+
+#Set default values
+dst_port=10000
+dst_addr=192.168.70.129
+frequency=0.1
+cc=cubic
+DL=false
+interface=oaitun_ue1
+iperf_duration=100s
+pcap_file_name=ue.pcap
+cc_file_name=ue_cc
+
+# Parse command line arguments
+while getopts ":p:a:f:c:d:i:t:x:y:" opt; do
+  case $opt in
+    p) dst_port=$OPTARG ;;
+    a) dst_addr=$OPTARG ;;
+    f) frequency=$OPTARG ;;
+    c) cc=$OPTARG ;;
+    d) DL=$OPTARG ;;
+    i) interface=$OPTARG ;;
+    t) iperf_duration=$OPTARG ;;
+    x) pcap_file_name=$OPTARG ;;
+    y) cc_file_name=$OPTARG ;;
+    \?) echo "Invalid option -$OPTARG" >&2 ;;
+  esac
+done
+
+echo "Capturing on $interface"
+echo "Dumping iperf session data in $pcap_file_name"
+
+tmux new-session -d -s "iperf_capture" 'tcpdump -i '"$interface"' -n tcp -s 88 -w '"$pcap_file_name"'; sleep 0.00001' 
+
+if "$DL"
+  then
+    tmux new-session -d -s "iperf_server" iperf3 -s -p "$dst_port"
+
+else
+  tmux new-session -d -s "cc" './ss_script.sh '"$dst_addr"' '"$frequency"' > '"$cc_file_name"'; sleep 0.00001'
+    sleep 1s
+  tmux new-session -d -s "iperf_client" iperf3 -c "$dst_addr" -t "$iperf_duration" -C "$cc" -p "$dst_port"
+  date +%H%M%S.%6N
+
+  echo "dst_port: $dst_port"
+  echo "dst_addr: $dst_addr"
+  echo "cc frequency: $frequency"
+  echo "cc alg: $cc"
+  echo "iperf duration: $iperf_duration"
+  echo "Dumping cc data in $cc_file_name"
+  echo "uplink"
+
+fi
+
+sleep "$iperf_duration"
+sleep 10s
+tmux kill-server
